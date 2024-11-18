dd79a1b8cd749073f0d17c72d4ca7c33a3dbe678
SameerKarodia <sameerkarodia@gmail.com>
2024-11-18 09:10:43 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -16,23 +16,54 @@ int main() {
     
     cout << "For signal 1 enter the following values: " << endl;
 
-    cout << "Amplitude in volts: " << endl;
-    cin >> amp1;
-    cout << "Frequency in radians: " << endl;
-    cin >> fre1;
-    cout << "Initial phase in radians: " << endl;
-    cin >> iniphase1;
-    
-    cout << "Do you want to input t1 or x1? " << endl;
-    cin >> test;  
+    do{
+        cout << "Amplitude in volts: " << endl;
+        cin >> amp1;
+        if (amp1 <= 0) {
+            cout << "Error: Amplitude must be greater than zero.\n";
+        }
+    }while(amp1<=0);
 
+
+    do{
+        cout << "Frequency in radians: " << endl;
+        cin >> fre1;
+        if (fre1 <= 0) {
+            cout << "Error: Frequency must be a positive value.\n";
+        }
+    }while(fre1<=0);
+
+    cout << "Initial phase in radians: " << endl;
+    cin >> iniphase1;  
+
+    //input handling for t1 and x1
+    do {
+        cout << "Do you want to input t1 or x1? (Enter 't1' or 'x1'): " << endl;
+        cin >> test;
+        if (test != "t1" && test != "x1") {
+            cerr << "Error: Invalid choice. Please enter 't1' or 'x1'.\n";
+        }
+    } while (test != "t1" && test != "x1");
+
+    // Time (t1) or Distance (x1)
     if (test == "t1") {
-        cout << "Enter t1: " << endl;
-        cin >> t1;
-    } else {
-        cout << "Enter x1: " << endl;
-        cin >> x1;     
+        do {
+            cout << "Enter t1 (must be non-negative): " << endl;
+            cin >> t1;
+            if (t1 < 0) {
+                cerr << "Error: t1 cannot be negative. Please try again.\n";
+            }
+        } while (t1 < 0);
+    } else if (test == "x1") {
+        do {
+            cout << "Enter x1 (must be non-negative): " << endl;
+            cin >> x1;
+            if (x1 < 0) {
+                cerr << "Error: x1 cannot be negative. Please try again.\n";
+            }
+        } while (x1 < 0);
     }
+
 
     //initialize other variables for signal 1
 
@@ -46,23 +77,51 @@ int main() {
     string test2;  
 
     cout << "For signal 2 enter the following values: " << endl;
+    do{
+        cout << "Amplitude in volts: " << endl;
+        cin >> amp2;
+        if (amp2 <= 0) {
+            cout << "Error: Amplitude must be greater than zero.\n";
+        }
+    }while(amp2<=0);
 
-    cout << "Amplitude in volts: " << endl;
-    cin >> amp2;
-    cout << "Frequency in radians: " << endl;
-    cin >> fre2;
+    do{
+        cout << "Frequency in radians: " << endl;
+        cin >> fre2;
+        if (fre2 <= 0) {
+            cout << "Error: Frequency must be a positive value.\n";
+        }
+    }while(fre2<=0);
+
     cout << "Initial phase in radians: " << endl;
     cin >> iniphase2;
     
-    cout << "Do you want to input t2 or x2? " << endl;
-    cin >> test2;  
+    //input handling for t2 and x2
+    do {
+        cout << "Do you want to input t2 or x2? (Enter 't2' or 'x2'): " << endl;
+        cin >> test2;
+        if (test2 != "t2" && test2 != "x2") {
+            cerr << "Error: Invalid choice. Please enter 't2' or 'x2'.\n";
+        }
+    } while (test2 != "t2" && test2 != "x2");
 
+    // Time (t2) or Distance (x2)
     if (test2 == "t2") {
-        cout << "Enter t2: " << endl;
-        cin >> t2;
-    } else {
-        cout << "Enter x2: " << endl;
-        cin >> x2;     
+        do {
+            cout << "Enter t2 (must be non-negative): " << endl;
+            cin >> t2;
+            if (t2 < 0) {
+                cerr << "Error: t2 cannot be negative. Please try again.\n";
+            }
+        } while (t2 < 0);
+    } else if (test2 == "x2") {
+        do {
+            cout << "Enter x2 (must be non-negative): " << endl;
+            cin >> x2;
+            if (x2 < 0) {
+                cerr << "Error: x2 cannot be negative. Please try again.\n";
+            }
+        } while (x2 < 0);
     }
 
     //initialize other variables for signal 2
@@ -70,6 +129,12 @@ int main() {
     double wavelength2=speedofLight/fre2;
     double angfre2=2*PI*fre2;
     double wavenum2=(2*PI)/wavelength2;
+
+
+    //calculate signals 1 and 2 with the equation 
+
+    double D1= amp1 * sin ( (wavenum1 * x1) + (angfre1 * t1) +iniphase1 );
+    double D2= amp2 * sin ( (wavenum2 * x2) + (angfre2 * t2) +iniphase2 );
 
     // Prepare data for plotting
     ofstream data("signals.dat"); // File to store the data
@@ -94,7 +159,7 @@ int main() {
     data.close();
 
     // Plotting using gnuplot
-    FILE* gnuplotPipe = _popen("C:\\\"Program Files\"\\gnuplot\\bin\\gnuplot.exe", "w");;
+    FILE *gnuplotPipe = _popen("C:\\\"Program Files\"\\gnuplot\\bin\\gnuplot.exe", "w");;
     if (gnuplotPipe) {
         fprintf(gnuplotPipe, "set title 'Signal Interference'\n");
         fprintf(gnuplotPipe, "set xlabel 'Distance or Time'\n");
